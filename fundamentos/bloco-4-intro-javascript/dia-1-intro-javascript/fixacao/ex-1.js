a4c02fb56485562b612b0622bd66cd7e6964b5ba
luc0liv <luoliveiradacunha@gmail.com>
2022-08-18 15:22:02 -0300

    exercícios de fixação intro javascript

---
+++ fundamentos/bloco-4-intro-javascript/dia-1-intro-javascript/fixacao/ex-1.js
@@ -0,0 +1,12 @@
+const myName = "Luciana";
+const birthCity = "Angra dos Reis";
+let birthYear = 1993;
+
+console.log(
+  "Meu nome é " +
+    myName +
+    " e eu nasci em " +
+    birthCity +
+    " no ano de " +
+    birthYear
+);
