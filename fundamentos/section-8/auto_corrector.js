49ab4efd2c9c6d1d8431f5ac57b186c4666944b6
luc0liv <luoliveiradacunha@gmail.com>
2022-09-23 12:09:26 -0300

    Melhorias

--- fundamentos/section-8/auto_corrector.js
+++ fundamentos/section-8/auto_corrector.js
@@ -1,26 +1,21 @@
-const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
-const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+const RIGHT_ANSWERS = ["A", "C", "B", "D", "A", "A", "D", "A", "D", "C"];
+const STUDENT_ANSWERS = ["A", "N.A", "B", "D", "A", "C", "N.A", "A", "D", "B"];
 
 const compareAnswers = (rightAnswers, studentAnswers) => {
-	let points = 0;
-  for (let index = 0; index < studentAnswers.length; index += 1) {
-		if (rightAnswers[index] === studentAnswers[index]) {
-			points += 1;
-		} else if(studentAnswers[index] === 'N.A') {
-			points += 0
-		}
-		else  {
-			points -= 0.5;
-		}
+    if (rightAnswers === studentAnswers) {
+      return 1;
+    } else if (studentAnswers === "N.A") {
+     return 0;
+    } 
+		return -0.5;
+};
 
-		console.log(points);
+const corrector = (rightAnswers, studentAnswers, callback) => {
+	let points = 0;
+	for (let index = 0; index < studentAnswers.length; index += 1) {
+		const callbackReturn = callback(rightAnswers[index], studentAnswers[index]);
+		points += callbackReturn;
 	}
 
-	return points;
-}
-
-const corrector = (rightAnswers, studentAnswers, callback ) => {
-return callback(rightAnswers, studentAnswers);
-}
-
-console.log(corrector(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
+  return `Pontos: ${points}`;
+};
