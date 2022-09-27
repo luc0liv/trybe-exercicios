6b86b1f005f08d64131d9ba3677d88fd17dc8473
luc0liv <luoliveiradacunha@gmail.com>
2022-09-27 20:40:06 -0300

    Exercícios 1 e 2

---
+++ fundamentos/section-8/dia-4/script.js
@@ -0,0 +1,108 @@
+// const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+// const isEven = (acc, number) => (number % 2 === 0 ? acc + number : acc);
+
+// const sumEvens = numbers.reduce(isEven, 0);
+
+// console.log(sumEvens);
+
+const estudantes = [
+    {
+      nome: 'Jorge',
+      sobrenome: 'Silva',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 67 },
+        { name: 'Português', nota: 79 },
+        { name: 'Química', nota: 70 },
+        { name: 'Biologia', nota: 65 },
+      ],
+    },
+    {
+      nome: 'Mario',
+      sobrenome: 'Ferreira',
+      idade: 15,
+      turno: 'Tarde',
+      materias: [
+        { name: 'Matemática', nota: 59 },
+        { name: 'Português', nota: 80 },
+        { name: 'Química', nota: 78 },
+        { name: 'Biologia', nota: 92 },
+      ],
+    },
+    {
+      nome: 'Jorge',
+      sobrenome: 'Santos',
+      idade: 15,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 76 },
+        { name: 'Português', nota: 90 },
+        { name: 'Química', nota: 70 },
+        { name: 'Biologia', nota: 80 },
+      ],
+    },
+    {
+      nome: 'Maria',
+      sobrenome: 'Silveira',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 91 },
+        { name: 'Português', nota: 85 },
+        { name: 'Química', nota: 92 },
+        { name: 'Biologia', nota: 90 },
+      ],
+    },
+    {
+      nome: 'Natalia',
+      sobrenome: 'Castro',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 70 },
+        { name: 'Português', nota: 70 },
+        { name: 'Química', nota: 60 },
+        { name: 'Biologia', nota: 50 },
+      ],
+    },
+    {
+      nome: 'Wilson',
+      sobrenome: 'Martins',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 80 },
+        { name: 'Português', nota: 82 },
+        { name: 'Química', nota: 79 },
+        { name: 'Biologia', nota: 75 },
+      ],
+    },
+  ];
+
+  const getBestGrade = (acc, subject) => acc.nota > subject.nota ? acc : subject;
+
+  const formatBestGrade = () => {
+    const students = estudantes.map(estudante => ({
+        name: estudante.nome,
+        materia: estudante.materias.reduce(getBestGrade).name
+    }));
+
+    return students;
+  }
+
+  console.log(formatBestGrade());
+
+
+  const seila = () => {
+    const numbers = [10, 24, 43, 76];
+    return numbers.reduce((acc, curr) => {
+        console.log('acc', acc);
+        console.log('curr', curr);
+        console.log('sub', curr - acc);
+        return curr - acc
+    });
+  }
+  
+  console.log(seila());
