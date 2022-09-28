0ca49b0eef7039d99e80dc0b6d53b1ecb03aae1b
luc0liv <luoliveiradacunha@gmail.com>
2022-09-28 20:31:01 -0300

    exercicios 8 e 9

---
+++ fundamentos/section-8/dia-5/ex-8.js
@@ -0,0 +1,10 @@
+// escreva greet abaixo
+
+const greet = (name, greeting = 'Hi') => {
+    return `${greeting}, ${name}!`
+}
+
+// Retornos esperados:
+console.log(greet('John')); // 'Hi John'
+console.log(greet('John', 'Good morning')); // 'Good morning John'
+console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
